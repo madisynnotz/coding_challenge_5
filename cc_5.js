e3cb4a2fa724147785297f06acb3982cbfdcfa6e
Madisyn Notz <madisynnotz33@gmail.com>
2025-02-03 17:00:24 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Object Properties
+const customer = {
+    name: "Amy Hill",
+    age: 19,
+    email: "emily.hill@email.com"
+};
+
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
